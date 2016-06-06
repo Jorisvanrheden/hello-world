ae8191fa542425779ee8799419ca7cc08b542069
Jorisvanrheden <jorisvanrheden@Hotmail.com>
2016-06-06 17:01:34 +0200

    Add files via upload

---
+++ Text.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Text : MonoBehaviour {
+
+	public GUIStyle style;
+
+	private string word = "Hallo schatje, alles goed? 我想要你親我";
+	private string write = "";
+
+	void Start(){
+		StartCoroutine ("test", 0);
+	}
+
+
+	void OnGUI(){
+		GUI.TextArea (new Rect (100, 100, 100, 100), write, style);
+	}
+
+	IEnumerator test(int i){
+		if (i < word.Length) {
+			write += word [i];
+			yield return new WaitForSeconds(0.05f);
+			StartCoroutine("test", i+1);
+		}
+		yield break;
+	}
+}
